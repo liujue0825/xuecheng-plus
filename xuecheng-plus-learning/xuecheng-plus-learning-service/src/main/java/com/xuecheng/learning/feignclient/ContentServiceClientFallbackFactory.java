50ef7b575392e6fab375ddbd5c703fae1171d53d
liujue <liujue@qq.com>
2024-02-20 18:39:38 +0800

    更新

--- xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/feignclient/ContentServiceClientFallbackFactory.java
+++ xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/feignclient/ContentServiceClientFallbackFactory.java
@@ -1,5 +1,7 @@
 package com.xuecheng.learning.feignclient;
 
+import com.xuecheng.content.model.po.CoursePublish;
+import com.xuecheng.content.model.po.Teachplan;
 import feign.hystrix.FallbackFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -12,9 +14,18 @@ import org.springframework.stereotype.Component;
 public class ContentServiceClientFallbackFactory implements FallbackFactory<ContentServiceClient> {
     @Override
     public ContentServiceClient create(Throwable throwable) {
-        return courseId -> {
-            log.error("调用内容管理服务发生熔断:{}", throwable.toString(),throwable);
-            return null;
+        return new ContentServiceClient() {
+            @Override
+            public CoursePublish getCoursePublish(Long courseId) {
+                log.error("调用内容管理服务发生熔断:{}", throwable.toString(), throwable);
+                return null;
+            }
+
+            @Override
+            public Teachplan getTeachplan(Long teachplanId) {
+                log.error("调用内容管理服务发生熔断:{}", throwable.toString(), throwable);
+                return null;
+            }
         };
     }
 }
