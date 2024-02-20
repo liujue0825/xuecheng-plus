60244ec2a353c7e3b456217cb61127d5d65232df
liujue <liujue@qq.com>
2024-02-20 18:42:30 +0800

    更新

--- xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/ContentServiceApplicationTests.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/ContentServiceApplicationTests.java
@@ -1,46 +1,24 @@
 package com.xuecheng;
 
-import com.xuecheng.base.model.PageParams;
-import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.ContentServiceApplication;
-import com.xuecheng.content.config.MultipartSupportConfig;
-import com.xuecheng.content.feignclient.MediaServiceClient;
 import com.xuecheng.content.mapper.CourseBaseMapper;
 import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
-import com.xuecheng.content.model.dto.CoursePreviewDto;
-import com.xuecheng.content.model.dto.QueryCourseParamDto;
 import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.service.CourseBaseInfoService;
 import com.xuecheng.content.service.CourseCategoryService;
-import com.xuecheng.content.service.CoursePublishService;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
 
 @Slf4j
 @SpringBootTest(classes = ContentServiceApplication.class)
 public class ContentServiceApplicationTests {
     @Resource
     CourseBaseMapper courseBaseMapper;
-
-    @Resource
-    CourseBaseInfoService courseBaseInfoService;
 
     @Resource
     CourseCategoryService courseCategoryService;
@@ -62,6 +40,5 @@ public class ContentServiceApplicationTests {
     public void test() {
         System.out.println("hello world");
     }
-
 
 }
